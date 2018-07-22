66664842ea691cad9b953edf0fa7d1fc6fcdb7ae
Keller Me <kellerme@KellerdeMBP.lan>
2018-07-22 17:37:30 +0800

    lazy loading

--- src/index.js
+++ src/index.js
@@ -1,29 +1,32 @@
-import printMe from './print.js';
+import _ from 'lodash';
 import './style.css';
 import './bk.jpg';
 import {cube} from './math';
 
 console.log(process.env.NODE_ENV);
 
-async function component() {
+function component() {
     var element = document.createElement('div');
-    const _ = await import(/* webpackChunkName: "lodash" */ "lodash");
+    // const _ = await import(/* webpackChunkName: "lodash" */ "lodash");
     var str = '5 cubed is ' + cube(5);
     element.innerHTML = _.join(['hello', 'mpp', str], ' ');
     element.classList.add('hello');
 
     var btn = document.createElement('button');
     btn.innerHTML = 'click me to check console';
-    btn.onclick = printMe;
     element.appendChild(btn);
+    btn.onclick = e => import(/* webpackChunkName: "print" */ "./print").then(module=>{
+        var print = module.default;
+        print();
+    });
     return element;
 }
 
 let element = component();
-element.then(component => {
-    document.body.appendChild(component);
-});
-// document.body.appendChild(element);
+// element.then(component => {
+//     document.body.appendChild(component);
+// });
+document.body.appendChild(element);
 
 // if (module.hot) {
 //     module.hot.accept('./print.js', function () {
--- src/print.js
+++ src/print.js
@@ -1,5 +1,4 @@
-export default function printMe() {
-    console.log('I get called from printjs');
-    console.log('I get called from printjs2');
-    console.log('I get called from printjs3');
+console.log('printjs module has loaded');
+export default () => {
+    console.log('Button clicked: here is blabla');
 }
