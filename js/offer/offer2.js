9110641bf99df0d62f57a9e935e2d9f2154c2b86
Keller Me <ppmiao0628@outlook.com>
2019-08-04 10:34:07 +0800

    offer

--- js/offer/offer2.js
+++ js/offer/offer2.js
@@ -15,6 +15,10 @@ Consider the following matrix:
 
 Given target = 5, return true.
 Given target = 20, return false.
+解题思路
+要求时间复杂度 O(M + N)，空间复杂度 O(1)。其中 M 为行数，N 为 列数。
+
+该二维数组中的一个数，小于它的数一定在其左边，大于它的数一定在其下边。因此，从右上角开始查找，就可以根据 target 和当前元素的大小关系来缩小查找区间，当前元素的查找区间为左下角的所有元素。
  */
 (function () {
     let arr = [
