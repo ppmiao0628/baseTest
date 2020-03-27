346a98ac082edc548f22cdf17a85b1ab947a467c
Keller Me <ppmiao0628@outlook.com>
2020-03-27 21:14:31 +0800

    停止音乐

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import './style.css';
 import './bk.jpg';
 import {cube} from './math';
-
 console.log(process.env.NODE_ENV);
 const fn = () => 1; // ES6箭头函数, 返回值为1
 let num = 3 ** 2; // ES7求幂运算符
--- src/print.js
+++ src/print.js
@@ -3,3 +3,24 @@ console.log('printjs module has loaded');
 export default () => {
     console.log('Button clicked: here is blabla');
 }
+
+
+// console.log('20200321');
+// var i = 0;
+// function foo(){
+//   i++;
+//   if(i>20){
+//     return;
+//   }
+//   console.log("foo");
+//   let time = setTimeout(()=>{
+//     console.log("setTimeout");
+//   },0);
+//   process.nextTick(()=>{
+//       clearTimeout(time);
+//   });
+// }   
+// setTimeout(foo, 2);
+// setTimeout(()=>{
+//   console.log("Other setTimeout");
+// },2);
