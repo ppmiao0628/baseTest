5edd00bb2dc73082b5dd7559ced5bb66d7d51c6c
Keller Me <ppmiao0628@outlook.com>
2020-08-18 21:49:56 +0800

    fetch 和xhr请求数据

--- upload/demo3/server.js
+++ upload/demo3/server.js
@@ -1,57 +1,64 @@
-const http = require('http');
-const koaStatic = require('koa-static')
-const path = require('path');
-const koa = require('koa2');
-const koaBody = require('koa-body');
-const fs = require('fs');
-// const cors = require('koa-cors')
+/**
+ * 服务入口
+ */
+var http = require('http');
+var koaStatic = require('koa-static');
+var path = require('path');
+var koaBody = require('koa-body');
+var fs = require('fs');
+var Koa = require('koa2');
 
-var app = new koa();
+
+var app = new Koa();
 var port = process.env.PORT || '8100';
 
-var uploadHost = `http://127.0.0.1:8080/uploads/`;
-console.log('uploadHost', uploadHost);
+var uploadHost= `http://localhost:${port}/uploads/`;
+
 app.use(koaBody({
-    formidable:{
-         //设置文件的默认保存目录，不设置则保存在系统临时目录下  os
+    formidable: {
+        //设置文件的默认保存目录，不设置则保存在系统临时目录下  
         uploadDir: path.resolve(__dirname, '../static/uploads')
     },
-    multipart: true// 支持文件上传
+    multipart: true // 支持文件上传
 }));
 
-//开启静态文件访问
-app.use(
-    koaStatic(path.resolve(__dirname, '../static'))
-);
+app.use(koaStatic(
+    path.resolve(__dirname, '../static')
+));
 
 //二次处理文件，修改名称
-app.use((ctx)=>{
+app.use((ctx) => {
     console.log(ctx.request.files);
-    var files = ctx.request.files&&ctx.request.files.f1;
-    var result = [];
+    
+    var files = ctx.request.files.f1;//得到上传文件的数组
+    var result=[];
     console.log(files);
-    if(!Array.isArray(files)){
+
+    if(!Array.isArray(files)){//单文件上传容错
         files=[files];
     }
-    files && files.forEach(file=>{
-        // console.log('file.path',file.path);
-        var path = file.path.replace(/\\/g, '/');
-        var fname = file.name;
+    files && files.forEach(item=>{
+        var path = item.path.replace(/\\/g, '/');
+        var fname = item.name;//原文件名称
         var nextPath = path + fname;
-        if(file.size>0&&path){
+        if (item.size > 0 && path) {
+            //得到扩展名
             var extArr = fname.split('.');
-            var ext = extArr[extArr.length-1];
-            nextPath = path+'.'+ext;
-            console.log('nextPath',nextPath);
+            var ext = extArr[extArr.length - 1];
+            var nextPath = path + '.' + ext;
+            //重命名文件
             fs.renameSync(path, nextPath);
-            result.push(uploadHost+nextPath.slice(nextPath.lastIndexOf('/')+1));
-        }
-    })
-    ctx.body =`{
-        "fileUrl":${JSON.stringify(result)}
-    }` ;
-});
 
+            result.push(uploadHost+ nextPath.slice(nextPath.lastIndexOf('/') + 1));
+        }
+    });
+    ctx.body = `{
+        "fileUrl":${JSON.stringify(result)}
+    }`;
+})
+/**
+ * Create HTTP server.
+ */
 var server = http.createServer(app.callback());
 server.listen(port);
-console.log('server start:',port);
+console.log('demo4 server start ......   ');
